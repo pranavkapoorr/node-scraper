4857dbb51f93aaa0d258fcd0e6e8b48a7b92b2ee
Pranav Kapoor <pranavkapoorr@gmail.com>
2019-04-11 23:09:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+
+app.get('/scrape', function(req, res){
+    // The URL we will scrape from - in our example Anchorman 2.
+
+    url = 'https://github.com/pranavkapoorr/repositories/';
+
+    // The structure of our request call
+    // The first parameter is our URL
+    // The callback function takes 3 parameters, an error, response status code and the html
+
+    request(url, function(error, response, html){
+
+        // First we'll check to make sure no errors occurred when making the request
+
+        if(!error){
+            // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
+
+            var $ = cheerio.load(html);
+
+            // Finally, we'll define the variables we're going to capture
+            var repositoriesp1, title, description, timeline, techstack;
+            $('#js-pjax-container').filter(function(){
+
+                // Let's store the data we filter into a variable so we can easily see what's going on.
+     
+                     var data = $(this);
+                     repositoriesPage1 = data.children().last().html();
+                     
+            var json = { title : "", release : "", rating : ""};
+            res.send(repositoriesPage1);
+            });
+            
+
+            
+            
+        }
+    })
+})
+
+app.listen('8081')
+console.log('Magic happens on port 8081');
